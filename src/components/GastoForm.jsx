bbc3d80ab5ae4ca659d89f6133998ec4065ad6de
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-07-09 19:11:21 -0500

    llenando datos de formulario a state

--- src/components/GastoForm.jsx
+++ src/components/GastoForm.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
+
+const initialState = {
+  nombre: '',
+  monto: 0,
+  categoria: ''
+};
 
 const GastoForm = (props) => {
   const { animarForm } = props;
+  const [gasto, setGasto] = useState(initialState);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setGasto(gast => ({
+      ...gast,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
 
   return (
-    <form className={`formulario ${animarForm ? 'animar' : 'cerrar'}`}>
+    <form
+      className={`formulario ${animarForm ? 'animar' : 'cerrar'}`}
+      onSubmit={handleSubmit}
+    >
       <legend>Nuevo gasto</legend>
       <div className='campo'>
         <label htmlFor="nombre">Nombre gasto</label>
         <input
           type="text"
           id="nombre"
+          name='nombre'
+          value={gasto.nombre}
+          onChange={handleChange}
         />
       </div>
       <div className='campo'>
-        <label htmlFor="cantidad">Monto</label>
+        <label htmlFor="monto">Monto</label>
         <input
           type="number"
-          id="cantidad"
+          id="monto"
+          name='monto'
+          value={gasto.monto}
+          onChange={handleChange}
         />
       </div>
       <div className='campo'>
         <label htmlFor="categoria">Categoría</label>
-        <select id="categoria">
+        <select
+          id="categoria"
+          name='categoria'
+          value={gasto.categoria}
+          onChange={handleChange}
+        >
           <option value="">-- Seleccione --</option>
           <option value="ahorro">Ahorro</option>
           <option value="comida">Comida</option>
