58b1eea573e55afdc2ac848873224987ec03579b
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-12-06 20:17:01 -0600

    formateando fecha

--- src/components/GastoItem.jsx
+++ src/components/GastoItem.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import { formatearFecha } from '../helpers/index';
 
 const GastoItem = (props) => {
   const { gasto } = props;
 
   return (
     <div className='gasto sombra'>
+
       <div className='contenido-gasto'>
         <div className='descripcion-gasto'>
           <p className='categoria'>{gasto.categoria}</p>
           <p className='nombre-gasto'>{gasto.nombre}</p>
+          <p className="fecha-gasto">
+            Agregado el: <span>{formatearFecha(gasto.fecha)}</span>
+          </p>
         </div>
       </div>
+
+      <p className='cantidad-gasto'>${Number(gasto.monto)}</p>
     </div>
   );
 };
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,3 +4,15 @@ export const generarId = () => {
 
   return random + fecha;
 };
+
+export const formatearFecha = (fecha) => {
+  const newFecha = new Date(fecha);
+
+  const opciones = {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit'
+  };
+
+  return newFecha.toLocaleDateString('es-ES', opciones);
+};
